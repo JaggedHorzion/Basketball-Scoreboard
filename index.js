518765fee5d2392be1bcf633335761e83717fd09
JaggedHorzion <rickrtimmers@gmail.com~echo>
2022-10-16 15:45:54 +1100

    Added Green Color Score Leader Feature

--- index.js
+++ index.js
@@ -7,20 +7,12 @@ document.getElementById("score-guest-el").textContent = scoreGuest
 let scoreHomeEl = document.getElementById("score-home-el")
 let scoreGuestEl = document.getElementById("score-guest-el")
 
-<<<<<<< HEAD
 let homeTitle = document.getElementById("home-title")
 let guestTitle = document.getElementById("guest-title")
-=======
-//let homeTitle = document.getElementsByClassName("home-title")
-
-
-
->>>>>>> 66b18bf3923b3f8296991d3fddc391d758045cea
 
 function freethrowHome() {
     scoreHome += 1
     scoreHomeEl.textContent = scoreHome
-<<<<<<< HEAD
    if (scoreHome >= scoreGuest) {
       homeTitle.style.color = "#00FF00";
       guestTitle.style.color = "#FFFFFF";
@@ -29,16 +21,12 @@ function freethrowHome() {
       homeTitle.style.color = "#FFFFFF";
       guestTitle.style.color = "#00FF00";
 }
-=======
-
->>>>>>> 66b18bf3923b3f8296991d3fddc391d758045cea
 }
  
  
  function midrangeHome() {
     scoreHome += 2
     scoreHomeEl.textContent = scoreHome
-<<<<<<< HEAD
     if (scoreHome >= scoreGuest) {
       homeTitle.style.color = "#00FF00";
       guestTitle.style.color = "#FFFFFF";
@@ -48,14 +36,10 @@ function freethrowHome() {
       guestTitle.style.color = "#00FF00";
 }
 }
-=======
- }
->>>>>>> 66b18bf3923b3f8296991d3fddc391d758045cea
  
   function longthreeHome() {
     scoreHome += 3
     scoreHomeEl.textContent = scoreHome
-<<<<<<< HEAD
     if (scoreHome >= scoreGuest) {
       homeTitle.style.color = "#00FF00";
       guestTitle.style.color = "#FFFFFF";
@@ -65,14 +49,10 @@ function freethrowHome() {
       guestTitle.style.color = "#00FF00";
 }
 }
-=======
- }
->>>>>>> 66b18bf3923b3f8296991d3fddc391d758045cea
  
  function freethrowGuest() {
     scoreGuest += 1
     scoreGuestEl.textContent = scoreGuest
-<<<<<<< HEAD
     if (scoreGuest >= scoreHome) {
       guestTitle.style.color = "#00FF00";
       homeTitle.style.color = "#FFFFFF";
@@ -82,15 +62,10 @@ function freethrowHome() {
       homeTitle.style.color = "#00FF00";
 }
 }   
-=======
-    
- }
->>>>>>> 66b18bf3923b3f8296991d3fddc391d758045cea
  
  function midrangeGuest() {
     scoreGuest += 2
     scoreGuestEl.textContent = scoreGuest
-<<<<<<< HEAD
     if (scoreGuest >= scoreHome) {
       guestTitle.style.color = "#00FF00";
       homeTitle.style.color = "#FFFFFF";
@@ -100,14 +75,10 @@ function freethrowHome() {
       homeTitle.style.color = "#00FF00";
 }
 }
-=======
- }
->>>>>>> 66b18bf3923b3f8296991d3fddc391d758045cea
  
   function longthreeGuest() {
     scoreGuest += 3
     scoreGuestEl.textContent = scoreGuest
-<<<<<<< HEAD
     if (scoreGuest >= scoreHome) {
       guestTitle.style.color = "#00FF00";
       homeTitle.style.color = "#FFFFFF";
@@ -127,7 +98,3 @@ function freethrowHome() {
 
 
 
-=======
- }
- 
->>>>>>> 66b18bf3923b3f8296991d3fddc391d758045cea
